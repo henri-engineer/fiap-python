d869efc126027d80624e197f418e51601ee79ab3
henri-engineer <rm564586@fiap.com.br>
2025-03-12 00:35:05 +0000

    exercicios-aula1-finalizado

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # fiap-python
-# Lista de Exercícios
-# 1. Faça um programa que exiba seu nome na tela.
-# 2. Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
-# 3. Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+## Aula 1 - Lista de Exercícios
+1. Faça um programa que exiba seu nome na tela.
+2. Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
+3. Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+4. Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+5. Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
+6. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
+7. Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do  novo salário.
+8. Faça um programa que solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o preço a pagar.
+9. Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+10. Escreva um programa que converta uma temperatura digitada em ºC em ºF. A fórmula para essa conversão é $F = ((9*C)/5)+32$
+11. Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por km rodado.
+12. Escreva um programa que receba 2 valores do tipo inteiro x e y, e calcule o valor de z: $`z = (x²+y²)/(x-y)²`$
+13. Escreva um programa que receba o salário de um funcionário (float) e retorne o resultado do novo salário com reajuste de 35%.
--- aula1.py
+++ aula1.py
@@ -44,3 +44,48 @@ aumento = float(input("Digite o percentual de aumento: "))
 print("Valor de Aumento: R$", salario*aumento/100)
 print("Novo Salário: R$",salario+(salario*aumento/100))
 print(" ")
+
+print("########## Exercício 8 ##########")
+print("Faça um programa que solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o preço a pagar.")
+precoMercadoria = float(input("Digite o preço da mercadoria: "))
+desconto = float(input("Digite o percentual de desconto: "))
+print("Valor de Desconto: R$", precoMercadoria*desconto/100)
+print("Preço a pagar: R$", precoMercadoria-(precoMercadoria*desconto/100))
+print(" ")
+
+print("########## Exercício 9 ##########")
+print("Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.")
+distancia = float(input("Informe a distância (em Km) a percorrer: "))
+velocidadeMedia = float(input("Informa a velocidade média (em Km/h) espearada para a viagem: "))
+tempo = distancia / velocidadeMedia
+print("Tempo estimado de viagem: %.2f" %tempo, "horas")
+print(" ")
+
+print("########## Exercício 10 ##########")
+print("Escreva um programa que converta uma temperatura digitada em ºC em ºF. A fórmula para essa conversão é `F = ((9 x C) / 5) + 32`")
+celsius = float(input("Informe a temperatura em ºC: "))
+farenheit = ((9*celsius)/5)+32
+print("A temperatura em ºF é: %.2f" %farenheit)
+print(" ")
+
+print("########## Exercício 11 ##########")
+print("Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por km rodado.")
+kmPercorrido = float(input("Informe a quantidade de Km percorrido: "))
+diarias = int(input("Digite a quantidade de diárias: "))
+valorAluguel = kmPercorrido*0.15 + diarias * 60
+print("O valor do aluguel do carro é R$%.2f" %valorAluguel)
+print(" ")
+
+print("########## Exercício 12 ##########")
+print("Escreva um programa que receba 2 valores do tipo inteiro x e y, e calcule o valor de z: z = (x**2 + y**2) / (x-y)**2`")
+x = int(input("Informe o valor de x: "))
+y = int(input("Informe o valor de y: "))
+z = (x**2 + y**2) / (x-y)**2
+print("O valor de Z é ",z)
+print(" ")
+
+print("########## Exercício 13 ##########")
+print("Escreva um programa que receba o salário de um funcionário (float) e retorne o resultado do novo salário com reajuste de 35%.")
+salarioFuncionario = float(input("Informe o valor do salário: "))
+print("O valor do salário com 35% de reajuste é R$",salarioFuncionario+(salarioFuncionario*0.35))
+print(" ")
